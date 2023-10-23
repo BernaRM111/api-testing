838944d72c81d00b634a809bd84c65c7c788ccad
BernaRM111 <motoberna222@gmail.com>
2023-10-22 18:26:38 -0600

    update gets

--- index.js
+++ index.js
@@ -19,7 +19,26 @@ app.get('/hours', (req, res, next) => {
 
 app.get('/size', (req, res, next) => {
 	res.sendfile("assets/1a_proxy_size.json");
-  });
+});
+
+app.get('/3a_test1', (req, res, next) => {
+	res.sendfile("assets/3a_test1.json");
+});
+
+//Issue #6
+app.get('/3a_test2', (req, res, next) => {
+	res.sendfile("assets/3a_test2.json");
+});
+
+//Issue #7
+app.get('/3a_test3', (req, res, next) => {
+	res.sendfile("assets/3a_testt3.json");
+});
+
+//Issue #8
+app.get('/3a_test4', (req, res, next) => {
+	res.sendfile("assets/3a_test4.json");
+});
 
 app.listen(port,  () => 
 	console.log('listening on port ' + port
